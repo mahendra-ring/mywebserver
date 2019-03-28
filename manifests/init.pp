12fdb2560d38e4c8b94ae4ac820e47281aa51236
Raghavendra068 <raghu.gangadhar@gmail.com>
2019-03-28 17:43:08 +0530

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,6 +7,6 @@
 class mywebserver {
 notify {"Initializing the mywebserver apache server automated configuration...":}
   class { 'mywebserver::install': }
-  class { 'mywebserver::vhost': }
+  class { 'mywebserver::vhosts': }
 #  class {'mywebserver::content':}
 }
